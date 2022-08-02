64ae1b90dd7803a27fe2870f540b1ce948205ba6
Eddie Hernandez <eddie.hernandez.us@gmail.com>
2022-08-01 21:04:48 -0400

    finalized and cleaned up project, awesome

--- src/handlers/getCustomer.ts
+++ src/handlers/getCustomer.ts
@@ -3,6 +3,7 @@ import CustomersController from '../controllers/CustomersController';
 import Customer from '../models/Customer'
 import CustomerMockRepo from '../repos/CustomerMockRepo';
 import ICustomerRepo from '../repos/ICustomerRepo';
+import HandlersLib from '../utils/HandlersLib';
 
 // Eventually this can be replaced by a factory.  For now just directly instantiating for testing purposes.
 const _customersRepo: ICustomerRepo = new CustomerMockRepo()
@@ -11,33 +12,17 @@ const _customersController: CustomersController = new CustomersController(_custo
 module.exports.main = async (event) => {
 
     const id: number = event.pathParameters?.id
-    if (!id) {
-        return {
-            statusCode: 404,
-            body: JSON.stringify({message: `Customer id is missing in path parameter. (i.e. /customers/123)`})
-        }
-    }
-    console.log(`id = ${id}`)
+    if (!id) 
+        return HandlersLib.handlerReponse(404, {
+            message: `Customer id is missing in path parameter. (i.e. /customers/123)`
+        })
 
-    // const response = customersDb.find(x => {return x.id == id})
     const response = _customersController.getCustomer(id)
-    console.log('response = ', response)
 
-
-    if (!response){
-        return {
-            statusCode: 404,
-            body: JSON.stringify({message: `Customer not found for id of ${id}`})
-        }
-    }
+    if (!response)
+        return HandlersLib.handlerReponse(404, {
+            message: `Customer not found for id of ${id}`
+        })
     
-    return {
-        headers: { 'content-type': 'application/json'},
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-            data: response
-            })
-    }
-
+    return HandlersLib.handlerReponse(200, response)
 }
